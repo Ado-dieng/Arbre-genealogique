7d34be864acab0cc3e7921d727b8dc89e718e7c6
Ado-dieng <adokaderdieng@gmail.com>
2021-03-30 01:30:13 +0200

    init

---
+++ database/migrations/2021_03_29_135756_create_personnes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonnesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personnes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('parent_id')->unsigned()->nullable(true);
+            $table->foreign('parent_id')->references('id')->on('personnes');
+            $table->bigInteger('order')->unsigned();
+            $table->string('nom');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personnes');
+    }
+}
